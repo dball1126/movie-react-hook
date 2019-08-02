e64e430718065dc84557a640cd12751595bc6dac
daniel <g.thirteen333@gmail.com>
2019-08-02 18:41:27 -0400

    issue with app but stabilized

--- movie-hooks/src/App.js
+++ movie-hooks/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import '../src/App.css';
+import Movie from './Movie';
+import Search from './Search';
 
 function App() {
+  // const [loading, setLoading] = useState(true);
+  // const [movies, setMovies] = useState([]);
+  // const [errorMessage, setErrorMessage] = useState(null);
+  
   return (
     <div className="App">
       <header className="App-header">
--- movie-hooks/src/Search.js
+++ movie-hooks/src/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = (props) => {
     const [searchValue, setSearchValue] = useState("");
